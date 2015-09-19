02ec2bb55318574c04cf067c6cff9e1144227623
Clarke Benedict Plumo <clarke.plumo@gmail.com>
2015-09-19 15:44:01 +0800

    Add datetime to start of logs

--- LogsTextArea.java
+++ LogsTextArea.java
@@ -1,6 +1,7 @@
 package coda;
 
 import javax.swing.*;
+import java.util.Date;
 
 public class LogsTextArea extends JTextArea {
 
@@ -8,5 +9,10 @@ public class LogsTextArea extends JTextArea {
         this.setEditable(false);
 
         this.setRows(5);
+    }
+
+    @Override
+    public void append(String message) {
+        super.append(String.format("[%s] %s", new Date(), message));
     }
 }
